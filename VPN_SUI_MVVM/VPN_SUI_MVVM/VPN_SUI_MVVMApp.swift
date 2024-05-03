5d856f0dbabd9675cb7075dca87015c88b95833e
Yevgeny Ly <globchenko16@gmail.com>
2024-05-03 13:22:15 +0300

    - app init

---
+++ VPN_SUI_MVVM/VPN_SUI_MVVM/VPN_SUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPN_SUI_MVVMApp.swift
+//  VPN_SUI_MVVM
+//
+//  Created by Евгений Л on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct VPN_SUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
